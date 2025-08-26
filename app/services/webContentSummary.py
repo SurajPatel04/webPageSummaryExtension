dbb59a6c861b29fb84de5d943ff9f4a90772d7b4
SurajPatel <surajpatel9390@gmail.com>
2025-08-26 20:12:23 +0530

    refactor(webContentSummary): code restructure without changing behavior

--- app/services/webContentSummary.py
+++ app/services/webContentSummary.py
@@ -1,21 +1,18 @@
 from dotenv import load_dotenv
 from sse_starlette.sse import EventSourceResponse
+from app.core.llm import llmFlash
 import os
-import asyncio
 
 load_dotenv()
 
 if not os.getenv("USER_AGENT"):
     os.environ["USER_AGENT"] = "MyLangChainApp/1.0 (contact: you@example.com)"
 
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_community.document_loaders import WebBaseLoader, YoutubeLoader
-from langchain_core.messages import AIMessage, SystemMessage, HumanMessage, ChatMessage
 
-llm = ChatGoogleGenerativeAI(
-    api_key = os.getenv("GOOGLE_API_KEY"),
-    model="gemini-2.5-flash",
-)
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.messages import SystemMessage, HumanMessage
+
+
 def pageContentSummary(url):
     async def streamSummary():
         link = url
@@ -24,9 +21,15 @@ def pageContentSummary(url):
 
         doc = loader.load()
         contentDoc = doc[0].page_content
-        system_prompt = """You work is to tell in short what is written. you will get content and the user query based on the content give user answer
-        Ouput should look like this
-        AI: your response
+        system_prompt = """You are an AI assistant. 
+        Your task is to read the provided content and generate a clear, concise summary in **bullet points**. 
+        Do not include unnecessary details. 
+        Output format:
+
+        AI:
+        - Point 1
+        - Point 2
+        - Point 3
         """
 
         query = f"Provide me whole content summery in points{contentDoc}"
@@ -35,7 +38,7 @@ def pageContentSummary(url):
             HumanMessage(content=query)
         ]
 
-        async for chunk in llm.astream(msg):
+        async for chunk in llmFlash.astream(msg):
                 yield {"data": chunk.content}
 
     return EventSourceResponse(streamSummary())
